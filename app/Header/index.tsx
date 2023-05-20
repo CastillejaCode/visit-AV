42788951032a4c2954d1f2781e60bf56bb875ad5
Julian Krzysiak <jkrzysiak13@gmail.com>
2023-05-20 13:15:42 -0700

    Add animation to landing page

--- app/Header/index.tsx
+++ app/Header/index.tsx
@@ -3,7 +3,7 @@ import { Bars3Icon } from "@heroicons/react/24/solid";
 
 export default function Header() {
   return (
-    <header className="fixed flex w-screen justify-between border-b border-gray-800 bg-gray-50/80 px-4 py-2">
+    <header className="fixed z-10 flex w-screen justify-between border-b border-gray-800 bg-slate-100 px-4 py-2">
       <h1 className="text-3xl ">visit AV</h1>
       <button className="aspect-square w-8">
         <Bars3Icon />
--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,40 @@
+"use client";
+
+import { useLayoutEffect, useRef } from "react";
+import { gsap } from "gsap";
+
 import Header from "./Header/index";
 
 export default function Home() {
+  const ref = useRef<HTMLDivElement>();
+  const tl = useRef<GSAPTimeline>();
+
+  useLayoutEffect(() => {
+    let ctx = gsap.context(() => {
+      tl.current = gsap
+        .timeline({ delay: 1 })
+        .from(".enjoy", { opacity: 0, y: -200, duration: 0.8 })
+        .from(".av", { opacity: 0 }, "<1.5");
+    }, ref);
+
+    return () => ctx.revert();
+  }, []);
+
   return (
     <>
       <Header />
-      <main>
+      <main ref={ref}>
         <section className="h-screen bg-[url('../public/poppies.jpg')] bg-auto bg-scroll bg-left-top">
           <article className="absolute left-1/2 top-14 flex h-44 w-full max-w-lg -translate-x-1/2 flex-col justify-around p-6 font-display">
-            <h2 className=" text-center text-3xl font-medium text-gray-800 lg:text-4xl  ">
+            <h2 className="enjoy text-center text-3xl font-medium text-gray-800 lg:text-4xl">
               Enjoy the Beauty
             </h2>
-            <h1 className="text-center text-[2.8rem] font-bold text-gray-800 lg:text-6xl">
+            <h1 className="av text-center text-[2.8rem] font-bold text-gray-800 lg:text-6xl">
               Antelope Valley
             </h1>
           </article>
         </section>
+        <section className="h-screen"></section>
       </main>
     </>
   );
